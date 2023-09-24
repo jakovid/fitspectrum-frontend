d4e6476e5ed5afcd4d5c97aea2c693f85d7e994f
jakovid <jacobk.dowdle@gmail.com>
2023-09-23 22:31:14 -0400

    started to style the component

--- pages/calculators/bmi.tsx
+++ pages/calculators/bmi.tsx
@@ -7,24 +7,28 @@ export default function bmi() {
     let [measurementType, setMeasurementType] = useState<MeasurementSystem>("imperial")
 
     return (
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex flex-col bg-gray-400/60 w-1/2 items-center justify-center p-4">
+      <div className="flex h-screen items-center justify-center bg-[#00BCD4]">
+        <div className="flex flex-col bg-[#FF4081]/60 w-2/3 items-center justify-center p-4 rounded-md gap-4">
             <h3 className="text-xl">BMI Calculator</h3>
             <div className="flex flex-row gap-4">
-                <span className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "metric" ? "bg-red-900 text-white" : ""}`}>Metric</span>
-                <span className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "imperial" ? "bg-red-900 text-white" : ""}`}>Imperial</span>
+                <span className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "metric" ? "bg-red-900 text-white" : "bg-white text-[#263238]"}`}>Metric</span>
+                <span className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "imperial" ? "bg-[#00BCD4] text-white" : "bg-white text-[#263238]"}`}>Imperial</span>
             </div>
-            <form>
-                <div>
+            <form className="flex flex-col gap-2">
+                <div className="flex w-full justify-end gap-2">
                     <label>sex: </label>
-                    <select id='user_sex' name="user_sex" className="text-black" required>
+                    <select id='user_sex' name="user_sex" className="text-[#263238] w-24" required>
                         <option value="female">female</option>
                         <option value="male">male</option>
                     </select>
                 </div>
-                <div>
+                <div className="flex gap-2">
                     <label>weight ({measurementType == "imperial" ? "lbs" : "kg"}):</label>
-                    <input type="number" className="text-black"></input>
+                    <input type="number" className="text-black w-24"></input>
+                </div>
+                <div className="flex gap-2">
+                    <label>height ({measurementType == "imperial" ? "lbs" : "kg"}):</label>
+                    <input type="number" className="text-black w-24"></input>
                 </div>
             </form>
         </div>
