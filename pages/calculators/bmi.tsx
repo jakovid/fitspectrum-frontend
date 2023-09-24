07fa96d3176635d8ab4f9e2c1c1888d1ec49db06
jakovid <jacobk.dowdle@gmail.com>
2023-09-24 10:30:00 -0400

    user can now change between metric and imperial. state updates what form items are displayed

--- pages/calculators/bmi.tsx
+++ pages/calculators/bmi.tsx
@@ -9,12 +9,16 @@ export default function bmi() {
     return (
       <div className="flex h-screen items-center justify-center bg-[#00BCD4]">
         <div className="flex flex-col bg-[#FF4081]/60 w-2/3 items-center justify-center p-4 rounded-md gap-4">
+
             <h3 className="text-xl">BMI Calculator</h3>
+
             <div className="flex flex-row gap-4">
-                <span className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "metric" ? "bg-red-900 text-white" : "bg-white text-[#263238]"}`}>Metric</span>
-                <span className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "imperial" ? "bg-[#00BCD4] text-white" : "bg-white text-[#263238]"}`}>Imperial</span>
+                <span onClick={() => setMeasurementType("metric")} className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "metric" ? "bg-[#00BCD4] text-white" : "bg-white text-[#263238]"}`}>Metric</span>
+                <span onClick={() => setMeasurementType("imperial")} className={`w-20 flex justify-center rounded-s-full rounded-e-full border-white border-2 ${measurementType == "imperial" ? "bg-[#00BCD4] text-white" : "bg-white text-[#263238]"}`}>Imperial</span>
             </div>
+
             <form className="flex flex-col gap-2">
+
                 <div className="flex w-full justify-end gap-2">
                     <label>sex: </label>
                     <select id='user_sex' name="user_sex" className="text-[#263238] w-24" required>
@@ -22,13 +26,19 @@ export default function bmi() {
                         <option value="male">male</option>
                     </select>
                 </div>
+
                 <div className="flex gap-2">
-                    <label>weight ({measurementType == "imperial" ? "lbs" : "kg"}):</label>
+                    <label>weight:</label>
                     <input type="number" className="text-black w-24"></input>
+                    <div>{measurementType == "imperial" ? "lbs" : "kg"}</div>
                 </div>
+
                 <div className="flex gap-2">
-                    <label>height ({measurementType == "imperial" ? "lbs" : "kg"}):</label>
-                    <input type="number" className="text-black w-24"></input>
+                    <label>height:</label>
+                    <input type="number" className="text-black w-12"></input>
+                    <div>{measurementType == "imperial" ? "ft." : "cm"}</div>
+                    <input type="number" className={`${measurementType == "imperial" ? "w-12" : "hidden"}`}></input>
+                    <div>{measurementType == "imperial" ? "in." : ""}</div>
                 </div>
             </form>
         </div>
