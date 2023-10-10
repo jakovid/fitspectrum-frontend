8049a92d4eb5929e31798e41b8e9d5c971a35000
jakovid <jacobk.dowdle@gmail.com>
2023-10-09 22:25:44 -0400

    metric bmi calculator done roughy

--- components/calculators/bmi/bmi-results-generator.tsx
+++ components/calculators/bmi/bmi-results-generator.tsx
@@ -1,20 +1,22 @@
 import { useState } from "react";
 
-export default function BmiResultsGenerator(){
-    type BMITypes = "underweight" | "normal" | "overweight" | "obese"
-    let [bmiType, setBmiType] = useState<BMITypes | null>(null);
+interface BmiResultsGeneratorProps {
+    score: number;
+  }
+
+export default function BmiResultsGenerator({ score }: BmiResultsGeneratorProps){
 
     return(
         <div className="flex flex-col">
 
             <div className="flex gap-1">
-                <div>Your BMI is</div>
-                <div className="font-bold">5</div>
+                <div>Your BMI is </div>
+                <div className="font-bold">{ score }</div>
             </div>
 
             <div className="flex gap-1">
-                <div>You are considered</div>
-                <div className="font-bold">{bmiType}</div>
+                <div>You are considered <div className="font-bold">{(score < 18.5 ? "underweight" : score < 24 ? "normal" : score < 30 ? "overwight" : "obese")}</div></div>
+                
             </div>
             
         </div>
